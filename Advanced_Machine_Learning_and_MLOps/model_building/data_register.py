398374af23d9ccad5cd73d1b507060be916ea5c6
shrikantpillay <shrikantpillay@gmail.com>
2026-06-21 12:51:41 +0530

    Update folder path for data upload in data_register.py

--- Advanced_Machine_Learning_and_MLOps/model_building/data_register.py
+++ Advanced_Machine_Learning_and_MLOps/model_building/data_register.py
@@ -23,7 +23,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # 2. Safely jump up one level to 'Advanced_Machine_Learning_and_MLOps' and into 'data'
 data_folder_path = os.path.normpath(os.path.join(current_dir, "..", "data"))
 api.upload_folder(
-    folder_path="Advanced_Machine_Learning_and_MLOps/data",
+    folder_path=data_folder_path,
     repo_id=repo_id,
     repo_type=repo_type,
 )
